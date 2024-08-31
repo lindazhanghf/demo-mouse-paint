4539e94ec0abc5f7bcf0f9697babed42911f6273
Linda Zhang <linda.zhang@dartmouth.edu>
2024-08-31 11:11:01 +0100

    <DrawLine> [testing code] add CircleCollider2D to line points

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -3,12 +3,19 @@ using NaughtyAttributes;
 
 public class DrawLine : MonoBehaviour
 {
+    public float LineWidth = 0.1f;
+
     private LineRenderer m_lineRenderer;
+    private Transform m_lineColliders;
+    private Vector3 m_previousColliderPos;
 
     private void Awake()
     {
         m_lineRenderer = GetComponent<LineRenderer>();
         m_lineRenderer.positionCount = 0;
+
+        m_lineColliders = new GameObject("Colliders").transform;
+        m_lineColliders.SetParent(transform);
     }
 
     public void Draw(Vector3 currentPos)
@@ -21,5 +28,15 @@ public class DrawLine : MonoBehaviour
         }
         m_lineRenderer.positionCount++;
         m_lineRenderer.SetPosition(m_lineRenderer.positionCount - 1, currentPos);
+
+
+        if (m_previousColliderPos == Vector3.zero) m_previousColliderPos = currentPos;
+        else if (Vector3.Distance(currentPos, m_previousColliderPos) > LineWidth * 2)
+        {
+            CircleCollider2D circleCollider = m_lineColliders.gameObject.AddComponent<CircleCollider2D>();
+            circleCollider.radius = LineWidth;
+            circleCollider.offset = currentPos;
+            m_previousColliderPos = currentPos;
+        }
     }
 }
